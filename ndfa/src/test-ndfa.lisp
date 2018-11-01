3b2fd29d86eed3eae195b57405ea4db8d9b5ec12
jimka2001 <jimka.issy@gmail.com>
2018-11-01 22:54:44 +0100

    updated tests

--- ndfa/src/test-ndfa.lisp
+++ ndfa/src/test-ndfa.lisp
@@ -21,7 +21,7 @@
 
 
 (defpackage :ndfa-test
-  (:use :cl :ndfa :scrutiny))
+  (:use :cl :ndfa :scrutiny :adjuvant))
 
 (in-package :ndfa-test)
 
@@ -29,44 +29,48 @@
   (run-package-tests :ndfa-test))
 
 (define-test ndfa/test-reduce-2
-  (let ((dfa (make-ndfa '((:label 0 :initial-p t
-			   :transitions ((:next-label 6 :transition-label t1)
-					 (:next-label 1 :transition-label t2)))
-			  (:label 4 :final-p t)
-			  (:label 1
-			   :transitions ((:next-label 2 :transition-label t3)))
-			  (:label 6
-			   :transitions ((:next-label 2 :transition-label t3)))
-			  (:label 2
-			   :transitions ((:next-label 3 :transition-label t4)
-					 (:next-label 5 :transition-label t5)))
-			  (:label 3
-			   :transitions ((:next-label 4 :transition-label t6)))
-			  (:label 5
-			   :transitions ((:next-label 4 :transition-label t6)))))))
-    ;; TODO add assertion
-    (reduce-state-machine dfa)))
+  (let* ((dfa (make-ndfa '((:label x0 :initial-p t
+			   :transitions ((:next-label x6 :transition-label t1)
+					 (:next-label x1 :transition-label t2)))
+			  (:label x4 :final-p t)
+			  (:label x1
+			   :transitions ((:next-label x2 :transition-label t3)))
+			  (:label x6
+			   :transitions ((:next-label x2 :transition-label t3)))
+			  (:label x2
+			   :transitions ((:next-label x3 :transition-label t4)
+					 (:next-label x5 :transition-label t5)))
+			  (:label x3
+			   :transitions ((:next-label x4 :transition-label t6)))
+			  (:label x5
+			   :transitions ((:next-label x4 :transition-label t6))))))
+	 (reduced-dfa (reduce-state-machine dfa)))
+    (assert-true (= 7 (length (states dfa))))
+    (assert-true (= 5 (length (states reduced-dfa))))
+
+))
 
 (define-test ndfa/test-reduce-3
-  (let ((dfa (make-ndfa '((:label A
-			   :initial-p t
-			   :transitions ((:next-label B :transition-label 0)
-					 (:next-label C :transition-label 1)))
-			  (:label B
-			   :transitions ((:next-label B :transition-label 0)
-					 (:next-label D :transition-label 1)))
-			  (:label C
-			   :transitions ((:next-label B :transition-label 0)
-					 (:next-label C :transition-label 1)))
-			  (:label D
-			   :transitions ((:next-label B :transition-label 0)
-					 (:next-label E :transition-label 1)))
-			  (:label E
-			   :final-p t
-			   :transitions ((:next-label B :transition-label 0)
-					 (:next-label C :transition-label 1)))))))
-    ;; TODO add assertion
-    (reduce-state-machine dfa)))
+  (let* ((dfa (make-ndfa '((:label A
+			    :initial-p t
+			    :transitions ((:next-label B :transition-label 0)
+					  (:next-label C :transition-label 1)))
+			   (:label B
+			    :transitions ((:next-label B :transition-label 0)
+					  (:next-label D :transition-label 1)))
+			   (:label C
+			    :transitions ((:next-label B :transition-label 0)
+					  (:next-label C :transition-label 1)))
+			   (:label D
+			    :transitions ((:next-label B :transition-label 0)
+					  (:next-label E :transition-label 1)))
+			   (:label E
+			    :final-p t
+			    :transitions ((:next-label B :transition-label 0)
+					  (:next-label C :transition-label 1))))))
+	 (reduced-dfa (reduce-state-machine dfa)))
+    (assert-true (= 4 (length (states reduced-dfa))))
+    ))
   
 
 (define-test ndfa/test-trim-1
